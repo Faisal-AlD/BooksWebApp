27f1478958d6d8252551265c284df738bc66270c
Faisal Al <67331331+Faisal-AlD@users.noreply.github.com>
2022-03-21 02:28:16 -0400

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksWebApp.Models
+{
+    public class Book
+    {
+        // Properties 
+        public int Id { get; set; }   
+        public string Books { get; set; }
+
+        public string Summaries { get; set; }
+
+        public Book() //Constructor 
+        {
+                
+        }
+    }
+}
